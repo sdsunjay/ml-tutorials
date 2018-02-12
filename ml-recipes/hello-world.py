42b411a96f71ed4ab6efa7fd177f73d1b2f59c8c
Sunjay Dhama <sdsunjay73@yahoo.com>
2018-02-11 17:15:29 -0800

    Added some comments

--- ml-recipes/hello-world.py
+++ ml-recipes/hello-world.py
@@ -1,4 +1,11 @@
+# Six lines of Python is all it takes to write your first machine learning program! In this episode,
+# we'll briefly introduce what machine learning is and why it's important. Then, we'll follow a
+# recipe for supervised learning (a technique to create a classifier from examples) and code it up.
+# https://www.youtube.com/watch?v=cKxRvEZd3Mw
+
 from sklearn import tree
+# 1 is apple
+# 0 is orange
 features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 labels = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier()
