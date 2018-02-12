6065088050034847985fb8eeab10cba1a342a175
Sunjay Dhama <sdsunjay73@yahoo.com>
2018-02-11 18:26:38 -0800

    Completed first machine learning recipe

--- ml-recipes/hello-world.py
+++ ml-recipes/hello-world.py
@@ -1,3 +1,5 @@
+# Hello World - Machine Learning Recipes #1
+
 # Six lines of Python is all it takes to write your first machine learning program! In this episode,
 # we'll briefly introduce what machine learning is and why it's important. Then, we'll follow a
 # recipe for supervised learning (a technique to create a classifier from examples) and code it up.
